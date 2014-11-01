328816788d6c4ef9c0b913ed67b01d0bfecee3cf
Ben Schwabe <bspymaster@gmail.com>
2014-11-01 14:42:09 -0500

    process inputs
    CHANGED:
    updated calculator.py to process the two inputs to add and subtract

--- src/calculator.py
+++ src/calculator.py
@@ -4,8 +4,19 @@
 calculator.py
 """
 
+
+def processOperator(operator,num1,num2):
+	#add
+	if  (operator == "add"):
+		return num1 + num2
+	#subtract
+	elif (operator == "subtract"):
+		return num1 - num2
+	
+#control var for stopping program
 done = False
 
+#MAIN LOOP
 while not done:
     num1 = raw_input("first number: ")
     num1 = float(num1)
@@ -15,3 +26,5 @@ while not done:
     
     num2 = raw_input("second number: ")
     num2 = float(num2)
+    
+    print(processOperator(operator,num1,num2))
