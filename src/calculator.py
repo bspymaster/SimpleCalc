b03b3daff0529734ec10fae06d25a35251a34823
Ben Schwabe <bspymaster@gmail.com>
2014-11-01 14:32:34 -0500

    gitignore update and source original post
    NEW:
    -added main file to project
    
    FIX:
    -updated gitignore toignore geany system files

---
+++ src/calculator.py
@@ -0,0 +1,17 @@
+#Ben Schwabe
+#practice with python, for python 2.7
+"""
+calculator.py
+"""
+
+done = False
+
+while not done:
+    num1 = raw_input("first number: ")
+    num1 = float(num1)
+
+    operator = raw_input("operation: ")
+    operator = str(operator)
+    
+    num2 = raw_input("second number: ")
+    num2 = float(num2)
